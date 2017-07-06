030aa66a3a09358cda04cbe7e86b7560100cf3d6
Zachary Robbins <xaqiri@outlook.com>
2017-07-06 14:16:06 -0500

    Modified project to use my pyRL library

--- src/game.py
+++ src/game.py
@@ -1,9 +1,9 @@
 import sys
 import os
 import pygame as pyg
-import colors
-import random_level_gen as rlg
-import fov
+import pyRL.colors as colors
+import pyRL.random_level_gen as rlg
+import pyRL.fov as fov
 
 from entity import *
 
@@ -20,11 +20,12 @@ TILE_DIMENSION = int(8*SCALE)
 window_size = window_width, window_height = 1600, 900
 SCREEN_CENTER = (window_width//2, window_height//2)
 screen = pyg.display.set_mode(window_size)
-dungeon_size = (500, 500)
+dungeon_size = (32, 32)
 player = Entity(x=1, y=1)
 entities = [player]
 fov = fov.FOV()
-dungeon = rlg.RandomLevelGen(level_width=dungeon_size[0], level_height=dungeon_size[1], max_rooms=50, room_min_size=5, room_max_size=15)
+colors = colors.Colors()
+dungeon = rlg.RandomLevelGen(level_width=dungeon_size[0], level_height=dungeon_size[1], max_rooms=100, room_min_size=4, room_max_size=6)
 dungeon.make_level(entities)
 sprites = dict(wall=pyg.image.load(os.path.join('..', 'assets', 'sprites',
                                               'wall.png')).convert(),
@@ -37,15 +38,15 @@ sprites = dict(wall=pyg.image.load(os.path.join('..', 'assets', 'sprites',
 
 # Goes through each sprite and sets a certain color to be transparent and scales it to the appropriate dimensions
 for i in sprites:
-    sprites[i].set_colorkey(colors.Colors.TRANS)
+    sprites[i].set_colorkey(colors.TRANS)
     sprites[i] = pyg.transform.scale(sprites[i], (TILE_DIMENSION, TILE_DIMENSION))
 
 def color_sprite(sprite, color):
     new_sprite = sprite.copy()
     new_sprite = pyg.PixelArray(new_sprite)
-    new_sprite.replace(colors.Colors.BLACK, color)
+    new_sprite.replace(colors.BLACK, color)
     new_sprite =  pyg.PixelArray.make_surface(new_sprite)
-    new_sprite.set_colorkey(colors.Colors.TRANS)
+    new_sprite.set_colorkey(colors.TRANS)
     return new_sprite
 
 def main():
@@ -84,22 +85,25 @@ def update(clock):
 
 def render():
     global fov
-    screen.fill(colors.Colors.GRAY)
-    explored_wall = color_sprite(sprites['wall'], colors.Colors.YELLOW)
-    explored_floor = color_sprite(sprites['floor'], colors.Colors.YELLOW)
+    screen.fill(colors.DRK_GRAY)
+    explored_wall = color_sprite(sprites['wall'], colors.DRK_BLUE)
+    explored_floor = color_sprite(sprites['floor'], colors.BLUE)
+    visible_wall = color_sprite(sprites['wall'], colors.DRK_YELLOW)
+    visible_floor = color_sprite(sprites['floor'], colors.YELLOW)
     for ex in fov.explored_tiles:
         if dungeon.level[ex[0]][ex[1]] == 1:
-            screen.blit(explored_wall, (32*ex[0]+screen_offset[0], 32*ex[1]+screen_offset[1]))
+            screen.blit(explored_wall, (TILE_DIMENSION*ex[0]+screen_offset[0], TILE_DIMENSION*ex[1]+screen_offset[1]))
         else:
-            screen.blit(explored_floor, (32*ex[0]+screen_offset[0], 32*ex[1]+screen_offset[1]))
+            screen.blit(explored_floor, (TILE_DIMENSION*ex[0]+screen_offset[0], TILE_DIMENSION*ex[1]+screen_offset[1]))
     for v in fov.visible_tiles:
-        if (v == (player.x, player.y)):
-            screen.blit(sprites['player'], (32*v[0]+screen_offset[0], 32*v[1]+screen_offset[1]))
         if dungeon.level[v[0]][v[1]] == 1:
-            screen.blit(sprites['wall'], (32*v[0]+screen_offset[0], 32*v[1]+screen_offset[1]))
+            screen.blit(visible_wall, (TILE_DIMENSION*v[0]+screen_offset[0], TILE_DIMENSION*v[1]+screen_offset[1]))
         else:
-            screen.blit(sprites['floor'], (32*v[0]+screen_offset[0], 32*v[1]+screen_offset[1]))
-    screen.blit(ui_font.render(str(('{:.2f}'.format(fps_counter))), 1, colors.Colors.GREEN), (0, 0))
+            screen.blit(visible_floor, (TILE_DIMENSION*v[0]+screen_offset[0], TILE_DIMENSION*v[1]+screen_offset[1]))
+        if (v == (player.x, player.y)):
+            screen.blit(sprites['player'], (TILE_DIMENSION*v[0]+screen_offset[0], TILE_DIMENSION*v[1]+screen_offset[1]))
+
+    screen.blit(ui_font.render(str(('{:.2f}'.format(fps_counter))), 1, colors.GREEN), (0, 0))
     pyg.display.flip()
 
 def move(x, y):
